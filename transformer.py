4adced42933194ae5c281d33b30a0312ab641b0c
M. A. Rahal <m.a.rahal@outlook.com>
2021-01-28 10:42:39 +0100

    snippet data extraction now works

--- transformer.py
+++ transformer.py
@@ -1,23 +1,27 @@
 import re
 from functions import all_sub_files, name_of
 
+
 #==================================================================================================
 #=== Globals ==================================================================================================
 #==================================================================================================
 
 source_folder = 'C:/Users/Mohamed/AppData/Roaming/Sublime Text 3/Packages/User/Snippets/python snippets'
-
-def main():
-    files = all_sub_files(source_folder)
-    # print(*[x[:110] for x in files], sep='\n')
-    # print(*[name_of(x) for x in files], sep='\n')
-    with open(files[0], 'r') as f:
-        snippet = Snippet().extract_data_from_snippet(f.read())
-
+output_file = 'results.py'
+ignored_files = []
 
 #==================================================================================================
 #=== Functions & Classes ==================================================================================================
 #==================================================================================================
+
+def main():
+    files = all_sub_files(source_folder)
+    for f in files:
+        snippet = Snippet().extract_data_from_snippet(f)
+
+
+
+
 
 class Snippet():
     def __init__(self, name = '', body = '', descripion = '', trigger = '', scope = 'source.python'):
@@ -26,19 +30,32 @@ class Snippet():
         self.descripion = descripion
         self.trigger = trigger
 
-    def extract_data_from_snippet(self, snippet_text):
+    def extract_data_from_snippet(self, snippet):
+        with open(snippet, 'r') as f:
+            snippet_text = f.read()
+        self.name = name_of(snippet)
         '''takes SublimeText3 snippet text and extracts data from it'''
-        pattern = r'''<snippet>\n<content>((?:.|\n)+?)</content>\n<tabTrigger>(.+?)</tabTrigger>\n<scope>(.+?)</scope>\n(?:<description>(.*?)</description>)?\n</snippet>'''
+        pattern = r'''<snippet>\n<content>((?:.|\n)+?)</content>\n<tabTrigger>(.+?)</tabTrigger>\n<scope>(.+?)</scope>\n(?:<description>(.*?)</description>\n)?</snippet>'''
         res = re.search(pattern, snippet_text)
-        if res:
-            print(res.group(0))
-            print(00000)
-        else:
-            print(11111)
+        if not res:
+            print(f"can't match file '{self.name}', skipped")
+            ignored_files.append(snippet)
+            return
+
+        # assign each one to it's value
+        self.body = res.group(1)
+        self.trigger = res.group(2)
+        self.scope = res.group(3)
+        self.description = res.group(4) if res.group(4) is not None and len(res.group(4)) > 0 else None
 
 
 
 
+
+def show_files(files):
+    print(*[x[:110] for x in files], sep='\n')
+    print(*[name_of(x) for x in files], sep='\n')
+    
         
 
 #==================================================================================================
