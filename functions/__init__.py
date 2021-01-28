a8ed93a5c67fb7ea4ab48af8079e7d626590b4ac
M. A. Rahal <m.a.rahal@outlook.com>
2021-01-28 01:36:09 +0100

    snippet class regular expression for DATA EXTRACTION still doesn't work

---
+++ functions/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+def all_sub_files(path):
+    inner_files = os.listdir(path)
+    onlyfiles = [os.path.join(path, f) for f in inner_files if os.path.isfile(os.path.join(path, f))]
+    for directory in [os.path.join(path,f) for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]:
+        onlyfiles.extend(all_sub_files(directory))
+    return onlyfiles
+
+def name_of(path):
+    return path.split('\\')[-1].split('.')[0]
--- transformer.py
+++ transformer.py
@@ -1,4 +1,5 @@
-import os
+import re
+from functions import all_sub_files, name_of
 
 #==================================================================================================
 #=== Globals ==================================================================================================
@@ -10,24 +11,36 @@ def main():
     files = all_sub_files(source_folder)
     # print(*[x[:110] for x in files], sep='\n')
     # print(*[name_of(x) for x in files], sep='\n')
-
-    
-
+    with open(files[0], 'r') as f:
+        snippet = Snippet().extract_data_from_snippet(f.read())
 
 
 #==================================================================================================
-#=== Functions ==================================================================================================
+#=== Functions & Classes ==================================================================================================
 #==================================================================================================
 
-def all_sub_files(path):
-    inner_files = os.listdir(path)
-    onlyfiles = [os.path.join(path, f) for f in inner_files if os.path.isfile(os.path.join(path, f))]
-    for directory in [os.path.join(path,f) for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]:
-        onlyfiles.extend(all_sub_files(directory))
-    return onlyfiles
+class Snippet():
+    def __init__(self, name = '', body = '', descripion = '', trigger = '', scope = 'source.python'):
+        self.name = name
+        self.body = body
+        self.descripion = descripion
+        self.trigger = trigger
 
-def name_of(path):
-    return path.split('\\')[-1].split('.')[0]
+    def extract_data_from_snippet(self, snippet_text):
+        '''takes SublimeText3 snippet text and extracts data from it'''
+        pattern = r'''<snippet>\n<content>((?:.|\n)+?)</content>\n<tabTrigger>(.+?)</tabTrigger>\n<scope>(.+?)</scope>\n(?:<description>(.*?)</description>)?\n</snippet>'''
+        res = re.search(pattern, snippet_text)
+        if res:
+            print(res.group(0))
+            print(00000)
+        else:
+            print(11111)
+
+
+
+
+        
+
 #==================================================================================================
 #==================================================================================================
 
